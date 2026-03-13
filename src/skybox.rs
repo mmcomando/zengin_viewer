3bceadbf891770bfd0189baec3f14b518cffe3f1
Michał Masiukiewicz <masiukiewicz.michal@gmail.com>
2026-03-13 16:17:27 +0100

    Fix skybox

--- src/skybox.rs
+++ src/skybox.rs
@@ -13,7 +13,7 @@ impl Plugin for SkyBoxPlugin {
         app.add_systems(Update, update_skybox_texture);
         app.add_systems(
             Update,
-            add_skybox_to_camera.run_if(|res: Res<Cubemap>| !res.is_loaded),
+            add_skybox_to_camera.run_if(|res: Res<Cubemap>| res.is_loaded),
         );
     }
 }
@@ -23,22 +23,7 @@ struct Cubemap {
     is_loaded: bool,
     image_handle: Handle<Image>,
 }
-
-fn add_skybox_to_camera(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    query: Query<Entity, (With<FreeCamera>, Without<Skybox>)>,
-) {
-    let skybox_handle = asset_server.load("cubemap.png");
-    for entity_id in query.iter() {
-        commands.entity(entity_id).insert(Skybox {
-            image: skybox_handle.clone(),
-            brightness: 1000.0,
-            ..default()
-        });
-    }
-}
-
+// Step 1 Insert Resource and loada skybox texture
 fn spawn_skybox(mut commands: Commands, asset_server: Res<AssetServer>) {
     let skybox_handle = asset_server.load("cubemap.png");
 
@@ -47,31 +32,46 @@ fn spawn_skybox(mut commands: Commands, asset_server: Res<AssetServer>) {
         image_handle: skybox_handle.clone(),
     });
 }
-
+// Step 2 Make 2d skybox texture be 3d texture
 fn update_skybox_texture(
     asset_server: Res<AssetServer>,
     mut images: ResMut<Assets<Image>>,
     mut cubemap: ResMut<Cubemap>,
-    mut skyboxes: Query<&mut Skybox>,
 ) {
-    if !cubemap.is_loaded && asset_server.load_state(&cubemap.image_handle).is_loaded() {
-        let image = images.get_mut(&cubemap.image_handle).unwrap();
-        // NOTE: PNGs do not have any metadata that could indicate they contain a cubemap texture,
-        // so they appear as one texture. The following code reconfigures the texture as necessary.
-        if image.texture_descriptor.array_layer_count() == 1 {
-            image
+    if cubemap.is_loaded {
+        return;
+    }
+    if !asset_server.load_state(&cubemap.image_handle).is_loaded() {
+        return;
+    }
+    let image = images.get_mut(&cubemap.image_handle).unwrap();
+    // NOTE: PNGs do not have any metadata that could indicate they contain a cubemap texture,
+    // so they appear as one texture. The following code reconfigures the texture as necessary.
+    if image.texture_descriptor.array_layer_count() == 1 {
+        image
                 .reinterpret_stacked_2d_as_array(image.height() / image.width())
                 .expect("asset should be 2d texture and height will always be evenly divisible with the given layers");
-            image.texture_view_descriptor = Some(TextureViewDescriptor {
-                dimension: Some(TextureViewDimension::Cube),
-                ..default()
-            });
-        }
+        image.texture_view_descriptor = Some(TextureViewDescriptor {
+            dimension: Some(TextureViewDimension::Cube),
+            ..default()
+        });
+    }
 
-        for mut skybox in &mut skyboxes {
-            skybox.image = cubemap.image_handle.clone();
-        }
+    cubemap.is_loaded = true;
+}
 
-        cubemap.is_loaded = true;
+// Step 3 when skybox texture is ready add skybox component to camera
+fn add_skybox_to_camera(
+    mut commands: Commands,
+    cubemap: Res<Cubemap>,
+    query: Query<Entity, (With<FreeCamera>, Without<Skybox>)>,
+) {
+    for entity_id in query.iter() {
+        commands.entity(entity_id).insert(Skybox {
+            // image: skybox_handle.clone(),
+            image: cubemap.image_handle.clone(),
+            brightness: 1000.0,
+            ..default()
+        });
     }
 }
